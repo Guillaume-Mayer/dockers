45507989d3d6b52cf6fea83a6a5e7533d8c4bb2a
Guillaume Mayer <gmayer@arkho.tech>
2017-04-07 19:21:30 -0300

    Various

--- cocha/hub/docker-entrypoint.sh
+++ cocha/hub/docker-entrypoint.sh
@@ -1,2 +1,3 @@
 /usr/share/filebeat/bin/filebeat -c /etc/filebeat/filebeat.yml -path.home /usr/share/filebeat -path.config /etc/filebeat -path.data /var/lib/filebeat -path.logs /var/log/filebeat &
+
 $JBOSS_HOME/bin/standalone.sh
